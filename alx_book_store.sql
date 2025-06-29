0bfa285c3d637dab44010cef9d7a511464b0b2c7
Donia Salah <doniasalah954@gmail.com>
2025-06-29 07:31:35 +0300

    Fix: table to schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,17 +23,17 @@ CREATE TABLE Customers  (
 );
 
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE  Order_Details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
